b24adaba01c48d13834346f4d4dec843767617c5
Mathijs Lemmers <mathijs750@gmail.com>
2019-08-31 16:54:36 +0200

    Add resource implementation for water and light

---
+++ geemdjem0819/Assets/scripts/mechanics/nutrients/Water.cs
@@ -0,0 +1,17 @@
+using interfaces;
+using UnityEngine;
+
+namespace mechanics.nutrients
+{
+    public class Water : Nutrient
+    {
+        protected override int ResourceMaximum => 50;
+
+        public override int Consume(int amountDemanded)
+        {
+            var given = Mathf.Min(ResourceStore, amountDemanded);
+            ResourceStore = Mathf.Clamp(ResourceStore - given, 0, ResourceMaximum);
+            return given;
+        }
+    }
+}
