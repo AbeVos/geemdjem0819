750cd7678c272183ce1e4f844af38e8ccf971d1a
Denise Feeleûs <d.feeleus@gmail.com>
2019-08-31 13:26:50 +0200

    Implement seed becoming undraggable

--- geemdjem0819/Assets/scripts/Draggable.cs
+++ geemdjem0819/Assets/scripts/Draggable.cs
@@ -1,60 +1,11 @@
 ﻿using UnityEngine;
 
-public class Draggable : MonoBehaviour
+public class Draggable : MonoBehaviour, IDraggable
 {
-    public float catchingDistance = 3f;
-    private bool isDragging = false;
-    private GameObject draggingObject;
+    public bool isDraggable { get; set; }
 
-    void Update()
+    protected void Start()
     {
-        if (Input.GetMouseButton(0))
-        {
-            if (!isDragging)
-            {
-                draggingObject = GetObjectFromMouseRaycast();
-                if (draggingObject)
-                {
-                    draggingObject.GetComponent<Rigidbody>().isKinematic = true;
-                    isDragging = true;
-                }
-            }
-            else if (draggingObject != null)
-            {
-                draggingObject.GetComponent<Rigidbody>().MovePosition(CalculateMouse3DVector());
-            }
-        }
-        else
-        {
-            if (draggingObject != null)
-            {
-                draggingObject.GetComponent<Rigidbody>().isKinematic = false;
-            }
-            isDragging = false;
-        }
+        isDraggable = true;
     }
-    private GameObject GetObjectFromMouseRaycast()
-    {
-        GameObject gameObject = null;
-        bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hitInfo);
-        if (hit)
-        {
-            if (hitInfo.collider.gameObject.GetComponent<Rigidbody>() &&
-                Vector3.Distance(hitInfo.collider.gameObject.transform.position,
-                transform.position) <= catchingDistance)
-            {
-                gameObject = hitInfo.collider.gameObject;
-            }
-        }
-        return gameObject;
-    }
-    private Vector3 CalculateMouse3DVector()
-    {
-        Vector3 v3 = Input.mousePosition;
-        v3.z = catchingDistance;
-        v3 = Camera.main.ScreenToWorldPoint(v3);
-        //Debug.Log(v3); //Current Position of mouse in world space
-        return v3;
-    }
-
 }
---
+++ geemdjem0819/Assets/scripts/GrowthStarter.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GrowthStarter : MonoBehaviour
+{
+    public float germinationTime = 5f;
+    public Draggable draggable;
+    public Rigidbody rigidBody;
+
+    private void Start()
+    {
+        draggable = gameObject.GetComponent<Draggable>();
+        rigidBody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (germinationTime <= 0)
+        {
+            OkDoei();
+        }
+    }
+
+    private void OkDoei()
+    {
+        Destroy(draggable);
+        rigidBody.isKinematic = true;
+        Destroy(this);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Soil")
+        {
+            germinationTime -= Time.deltaTime * 1f;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        germinationTime = 5f;
+    }
+}
