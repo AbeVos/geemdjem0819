67fd48ab84cb103aece4dc7308d78d342d439564
Mathijs Lemmers <mathijs750@gmail.com>
2019-08-31 16:54:49 +0200

    Create GameManager.cs

---
+++ geemdjem0819/Assets/scripts/managers/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using interfaces;
+using UnityEngine;
+
+namespace managers
+{
+    public class GameManager : MonoBehaviour
+    {
+        /// <summary>
+        /// The time in seconds it takes before a Tick takesPlace.
+        /// </summary>
+        public int tickRate = 1;
+
+        private bool isTicking;
+
+         private List<ITickable> Tickables { get; set; }
+
+        private void Awake()
+        {
+            StartCoroutine(TickClock());
+        }
+
+
+        IEnumerator TickClock()
+        {
+            foreach (var tickable in Tickables)
+            {
+                tickable.Tick();
+            }
+
+            yield return new WaitForSeconds(tickRate);
+            if (isTicking) StartCoroutine(TickClock());
+        }
+    }
+}
