b4e482936470090cddcabcbe8201d4086d77159d
Denise Feeleûs <d.feeleus@gmail.com>
2019-08-31 16:44:58 +0200

    start implementation of daytimeslider

---
+++ geemdjem0819/Assets/scripts/DayNightSlider.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class DayNightSlider : MonoBehaviour
+{
+    public float timeOfDay;
+    public int directionalLightMinX;
+    public int directionalLightMaxX;
+    public Light directionalLight;
+
+    // The target marker.
+    public Transform target;
+
+    // Angular speed in radians per sec.
+    float speed= 0.5f;
+
+    void Update()
+    {
+        directionalLight.transform.LookAt(target);
+    }
+}
